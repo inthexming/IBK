6ab0170df917b1ee1191ca2665a1a3b94b352549
inthexming <90820612+inthexming@users.noreply.github.com>
2021-11-11 11:37:04 +0900

    update 로그인 페이지

---
+++ js/common/leftmenu.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    
+    // left 메뉴
+    $('.nav_cont_list > li > a').click(function(){
+        //$('.nav_cont_list li a').removeClass('active');
+        $(this).addClass('active');
+        $(this).next().slideDown(300);
+        $(".nav_cont_list > li > a").not(this).removeClass('active');
+        $(".nav_cont_list > li > a").not(this).next().slideUp(300);
+        return false;
+    });
+    
+    // select_box 
+    $('.selected-type').click(function(){
+        $(this).next().toggleClass('show');
+    });
+    $('.select_list li').click(function(){
+        $('.select_list').removeClass('show');
+    });
+
+    // 별 아이콘 
+    $('.mark_btn').click(function(){
+        $(this).toggleClass('active');
+    });
+
+});
